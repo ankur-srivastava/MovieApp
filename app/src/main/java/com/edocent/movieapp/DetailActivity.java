cc573c63e8446d1e273a1bb98a63abc744a6025f
Ankur Srivastava <ankurlkw@gmail.com>
2015-10-22 22:04:50 +0530

    With the current changes you can see the images displayed using a grid view

--- app/src/main/java/com/edocent/movieapp/DetailActivity.java
+++ app/src/main/java/com/edocent/movieapp/DetailActivity.java
@@ -1,10 +1,13 @@
 package com.edocent.movieapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class DetailActivity extends AppCompatActivity {
@@ -15,4 +18,25 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.content_detail);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
