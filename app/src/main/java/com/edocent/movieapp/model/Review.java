5b6986c0424655d82f78d5d7c2ec431f527efb0f
Ankur Srivastava <ankurlkw@gmail.com>
2015-10-26 11:19:09 +0530

    Added new Adapter for Reviews

---
+++ app/src/main/java/com/edocent/movieapp/model/Review.java
@@ -0,0 +1,44 @@
+package com.edocent.movieapp.model;
+
+/**
+ * Created by SRIVASTAVAA on 10/26/2015.
+ */
+public class Review {
+
+    long reviewId;
+    String author;
+    String content;
+    String url;
+
+    public long getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(long reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
